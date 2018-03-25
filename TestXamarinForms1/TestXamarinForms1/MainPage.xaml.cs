c03e3832881f7acc4b131edb2391fe19a251bebb
Colton Gardner <ColtonBootCamp@DESKTOP-BBU8CVU>
2018-03-25 14:00:01 -0700

    Add project files.

---
+++ TestXamarinForms1/TestXamarinForms1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestXamarinForms1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+		}
+
+        public void OnSave(object sender, EventArgs e)
+        {
+            LabelMessage.Text = "You clicked the damn button...";
+        }
+	}
+}
---
+++ TestXamarinForms1/TestXamarinForms1/Model/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestXamarinForms1.Model
+{
+    public class User
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        string Address { get; set; }
+    }
+
+    public class Owner : User
+    {
+        List<ParkingSpot> ParkingSpots { get; set; }
+
+        public class ParkingSpot
+        {
+            int ID { get; set; }
+            bool AvailableForRent { get; set; }
+            int HoursAvailable { get; set; }
+            int? RenterId { get; set; }
+        }
+
+    }
+
+    public class Driver : User
+    {
+        int ID { get; set; }
+        string LicenscePlate { get; set; }
+    }
+}
